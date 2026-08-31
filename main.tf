7b7fd3782da9de1080e9a93738074748701d9f8a
SaiPrinto <saikumarade@Saikumars-Laptop-2.local>
2026-08-31 13:17:19 +0530

    uncommited the changes

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
-module "aws_vpc" {
-  source      = "./modules/vpc"
-  vpc_name    = "sphere-vpc"
-  vpc_cidr    = "10.0.0.0/16"
-  environment = "dev"
+ module "aws_vpc" {
+   source      = "./modules/vpc"
+   vpc_name    = "sphere-vpc"
+   vpc_cidr    = "10.0.0.0/16"
+   environment = "dev"
 
-}
+ }
 
-module "secuirity-groups" {
-  source       = "./modules/security-groups"
-  vpc_id       = module.aws_vpc.vpc_id
-  environment  = var.environment
+ module "secuirity-groups" {
+   source       = "./modules/security-groups"
+   vpc_id       = module.aws_vpc.vpc_id
+   environment  = var.environment
   project_name = var.project_name
 
 
 
-}
+ }
 
+module "aws_s3_bucket" {
+  source       = "./modules/s3"
+}
